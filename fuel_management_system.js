931f81872ae953fe8f52e16efd0b940e5f878b81
Étienne Charignon <etienne.charignon@gmail.com>
2019-12-03 22:25:04 +0100

    day 3 part 2

--- fuel_management_system.js
+++ fuel_management_system.js
@@ -1,9 +1,20 @@
 const intersections= function (path1, path2) {
-    return path1.filter(p => path2.find(p2 => p2.x==p.x && p2.y==p.y));
+    var intersection = [];
+    path1.forEach(p => {
+      const p2 = path2.find(p2 => p2.x==p.x && p2.y==p.y);
+      if(p2) {
+        intersection.push({x:p.x, y:p.y, steps1:p.steps, steps2:p2.steps});
+      }
+    });
+    return intersection;
   };
 
 const distances_intersections= function (path1, path2) {
     return intersections(path1, path2).map(p => p.x + p.y);
+  };
+
+const steps_intersections= function (path1, path2) {
+    return intersections(path1, path2).map(p => p.steps1 + p.steps2);
   };
 
 module.exports = {
@@ -11,6 +22,7 @@ module.exports = {
     var points = []
     var x = 0;
     var y = 0;
+    var steps = 0;
     path.split(',').forEach((command) => {
       const instruction = command.match(/(.)(\d+)/)
       const direction = instruction[1];
@@ -20,7 +32,8 @@ module.exports = {
         if(direction== 'L') x--;
         if(direction== 'U') y++;
         if(direction== 'D') y--;
-        points.push({x, y});
+        steps++;
+        points.push({x, y, steps});
 
       }
     });
@@ -28,8 +41,12 @@ module.exports = {
   },
 
   intersections: intersections,
+
   distances_intersections: distances_intersections,
-  closest_disctances_intersections: function (path1, path2) {
-    return Math.min(...distances_intersections(path1, path2));
+
+  steps_intersections: steps_intersections,
+
+  closest: function (path1, path2, f) {
+    return Math.min(...f(path1, path2));
   }
 };

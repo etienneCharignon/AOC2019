5f4bb23da49200d531d9b8e88f72ad7190a2e662
Étienne Charignon <etienne.charignon@gmail.com>
2019-12-17 21:35:35 +0100

    day 16 phase 2 : fail

--- day16-flawed-frequency-transmission.js
+++ day16-flawed-frequency-transmission.js
@@ -1,27 +1,41 @@
-function elementPattern (row, input, basePattern) {
-  const elementPattern = [];
-  var bpI = 0;
-  for (var c=0; c < input.length + 1; c += row) {
-    for(var n=0; n<row; n++) {
-      elementPattern.push(basePattern[bpI%basePattern.length]);
-    }
-    bpI++;
-  }
-  return elementPattern.slice(1,input.length + 1);
+function patternIndex(i, row) {
+  return Math.floor((i+1) / row) % 4;
 }
 
-function computeElement(input, pattern) {
-  const sum = input.map((e,i) => e * pattern[i]).reduce((s, e) => s+e);
+function computeElementFromBase(row, input, basePattern, repetition = 1) {
+  var sum = 0;
+  for(var i = row-1; i < input.length;i++) {
+   // for(var p = 0; p < row && i < input.length; p++) {
+   //   sum += input[i];
+   //   i++;
+   // }
+   // i += row;
+   // for(var m = 0; m < row && i < input.length; m++) {
+   //   sum -= input[i];
+   //   i++;
+   // }
+   // i += row;
+    const pattern = basePattern[patternIndex(i,row)];
+    // if(pattern == 0) {
+    //   i += row -1;
+    //   continue;
+    // }
+    sum += input[i] * pattern;
+  }
+  sum *= repetition;
   return parseInt(sum.toString().split('').pop());
 }
 
-function compute(generation, input, basePattern) {
-  return input
-    .map((e, i) => computeElement(input, elementPattern(i+1, input,basePattern)));
+function compute(input, repetition, basePattern) {
+  const result = [];
+  for(var i = 1; i <= input.length; i++) {
+    result.push(computeElementFromBase(i, input, basePattern, repetition));
+  }
+  return result;
 }
 
 module.exports = {
-  elementPattern,
-  computeElement,
-  compute
+  compute,
+  computeElementFromBase,
+  patternIndex
 }
--- test/day16-flawed-trequency-transmission.js
+++ test/day16-flawed-trequency-transmission.js
@@ -1,33 +1,87 @@
 const expect = require('expect.js');
+const Stopwatch = require("node-stopwatch").Stopwatch;
 const basePattern = [ 0, 1, 0, -1 ];
-const { elementPattern, computeElement,compute } = require('../day16-flawed-frequency-transmission');
+const {
+  computeElementFromBase,
+  compute,
+  patternIndex
+} = require('../day16-flawed-frequency-transmission');
 
-describe.only('day 16 - Flawed Frequency Transmission', function () {
-  it('compute the first element pattern', function () {
-    const input = "12345678";
-    expect(elementPattern(1,input, basePattern)).to.eql([1,0,-1,0,1,0,-1, 0]);
+const puzzelInput = '59775675999083203307460316227239534744196788252810996056267313158415747954523514450220630777434694464147859581700598049220155996171361500188470573584309935232530483361639265796594588423475377664322506657596419440442622029687655170723364080344399753761821561397734310612361082481766777063437812858875338922334089288117184890884363091417446200960308625363997089394409607215164553325263177638484872071167142885096660905078567883997320316971939560903959842723210017598426984179521683810628956529638813221927079630736290924180307474765551066444888559156901159193212333302170502387548724998221103376187508278234838899434485116047387731626309521488967864391';
+
+describe('day 16 - Flawed Frequency Transmission', function () {
+
+  it('compute the pattern index for the first element', function () {
+    expect([0,1,2,3,4].map(i=> patternIndex(i, 1))).to.eql([1,2,3,0,1]);
   });
-  it('compute the second element pattern', function () {
-    const input = "12345678";
-    expect(elementPattern(2,input, basePattern)).to.eql([0,1,1,0,0,-1,-1,0]);
+
+  it('compute the pattern index for the second element', function () {
+    expect([0,1,2,3,4,5,6,7,8].map(i=> patternIndex(i, 2))).to.eql([0,1,1,2,2,3,3,0,0]);
   });
-  it('comute first element fist generation', function () {
+
+  it('compute the pattern index for the third element', function () {
+    expect([0,1,2,3,4,5,6,7,8].map(i=> patternIndex(i, 3))).to.eql([0,0,1,1,1,2,2,2,3]);
+  });
+
+  it('compute first element fist generation from basePattern', function () {
     const input = "12345678".split('').map(c => parseInt(c));
-    expect(computeElement(input, elementPattern(1, input, basePattern))).to.eql(4);
+    expect(computeElementFromBase(1, input, basePattern)).to.eql(4);
+  });
+
+  it('compute second element fist generation from basePattern', function () {
+    const input = "12345678".split('').map(c => parseInt(c));
+    expect(computeElementFromBase(2, input, basePattern)).to.eql(8);
+  });
+
+  it('compute third element fist generation from basePattern', function () {
+    const input = "12345678".split('').map(c => parseInt(c));
+    expect(computeElementFromBase(3, input, basePattern)).to.eql(2);
   });
 
   it('compute first generation', function () {
     const input = "12345678".split('').map(c => parseInt(c));
-    expect(compute(1, input, basePattern)).to.eql([4,8,2,2,6,1,5,8]);
+    expect(compute(input, 1, basePattern)).to.eql([4,8,2,2,6,1,5,8]);
+    expect(compute([4,8,2,2,6,1,5,8], 1, basePattern)).to.eql([3,4,0,4,0,4,3,8]);
   });
 
-  it('compute the puzzel input', function () {
-    const puzzelInput = '59775675999083203307460316227239534744196788252810996056267313158415747954523514450220630777434694464147859581700598049220155996171361500188470573584309935232530483361639265796594588423475377664322506657596419440442622029687655170723364080344399753761821561397734310612361082481766777063437812858875338922334089288117184890884363091417446200960308625363997089394409607215164553325263177638484872071167142885096660905078567883997320316971939560903959842723210017598426984179521683810628956529638813221927079630736290924180307474765551066444888559156901159193212333302170502387548724998221103376187508278234838899434485116047387731626309521488967864391'.split('').map(c => parseInt(c));
+  it('compute a larger input', function () {
+    const input = "80871224585914546619083218645595";
+
+    var signal = input;
+    for(var i = 0; i< 100;i++) {
+      signal = compute(signal, 1, basePattern);
+    }
+    expect(signal.slice(0,8).join('')).to.eql('24176176');
+  });
+
+  it('compute puzzel input phase 1', function () {
 
     var signal = puzzelInput;
-    for(var i = 0; i<100;i++) {
-      signal = compute(i, signal, basePattern);
+    for(var i = 0; i< 100;i++) {
+      signal = compute(signal, 1, basePattern);
     }
-    expect(signal.slice(0,8).join('')).to.eql('');
+    expect(signal.slice(0,8).join('')).to.eql('25131128');
+  });
+
+  xit('compute the puzzel input phase 2', function () {
+
+    var signal = ''
+    for(var n = 0; n <1; n++) {
+      signal += puzzelInput;
+    }
+
+    signal = signal.split('').map(c => parseInt(c));
+
+    console.log("ready")
+    const stopwatch = Stopwatch.create();
+    stopwatch.start();
+    for(var i = 0; i< 100;i++) {
+      signal = compute(signal, 100, basePattern);
+    }
+    stopwatch.stop();
+    console.log(stopwatch.elapsed.seconds);
+    console.log(parseInt(signal.slice(0,7).join('')));
+    const offset = parseInt(signal.slice(0,7).join(''));
+    expect(signal.slice(offset,offset+8).join('')).to.eql('25131128');
   });
 });
